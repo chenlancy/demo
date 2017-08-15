0a1b9e2fefcd969322bb27a5dbf32eeb30f7581f
jet <jet.qin@shinetechchina.com>
2017-08-15 10:43:18 +0800

    add docker file
    add docker file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @SpringBootApplication
@@ -10,7 +12,7 @@ public class DemoApplication {
 	@RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello Spring Boot Docker!";
 	}
 	
 	public static void main(String[] args) {
